e14c07debf6642db80dbe59b38e70dc8b8dd86b7
ASUSVIVOBOOK <ASUSVIVOBOOK@LAPTOP-K5NNEQ33>
2018-08-02 07:47:24 +0700

    For While Do-While

---
+++ src/j08/Contoh01.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j08;
+
+/**
+ *
+ * @author ASUSVIVOBOOK
+ */
+public class Contoh01 {
+    public static void main(String[] args) {
+    
+           
+        //PENGULANGAN FOR
+        int x;
+        for(x = 1; x <= 5; x++)
+        {
+            System.out.println("SMK MOKLET");
+        }
+        
+        //PENGULANGAN WHILE
+        int i = 1;
+        while(i <= 5)
+        {
+            System.out.println("SMK MOKLET");
+            i++;
+        }
+        
+        //PENGULANGAN DO WHILE
+        int j = 1;
+        do
+        {
+            System.out.println("SMK TELKOM");
+            j++;
+        }
+        while(j <= 5);
+        
+        
+        
+    }
+}
